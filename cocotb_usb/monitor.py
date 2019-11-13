21abe24b3d848cb608a280129ce3bf76bdaa9cc8
Rafal Kapuscik <rkapuscik@antmicro.com>
2019-11-13 13:45:30 +0100

    Clean up state variables
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- cocotb_usb/monitor.py
+++ cocotb_usb/monitor.py
@@ -14,17 +14,20 @@ class UsbMonitor(BusMonitor):
     Args:
         oversampling (int): How many times the signal is sampled on each cycle.
     """
+    # Internal states
+    (IDLE, PRIMED, RECEIVING) = range(3)
+
     def __init__(self, *args, **kwargs):
         self.cycles = kwargs.pop('oversampling', 4)
         self.clock_period = kwargs.pop('clk_period', 20830)  # 48 MHz
 
         self.dut = args[0]
         BusMonitor.__init__(self, *args, **kwargs)
-        self.primed = False
+        self.state = self.IDLE
 
     def prime(self):
         """Notify the object that a transaction is expected"""
-        self.primed = True
+        self.state = self.PRIMED
 
     @coroutine
     def _monitor_recv(self):
@@ -47,9 +50,10 @@ class UsbMonitor(BusMonitor):
             else:
                 raise TestFailure("Unrecognized dut values: {}".format(values))
 
-        rcv = False
         # We want to sample in the middle of a signal to allow for jitter
         t_middle = Timer(self.clock_period // 4, 'ps')
+        bit_time_max = 12.5
+        bit_time_acceptable = 7.5
         while True:
             yield RisingEdge(self.clock)
             yield t_middle
@@ -57,37 +61,38 @@ class UsbMonitor(BusMonitor):
                 continue
 
             # If someone is waiting for response, measure bit times
-            if self.primed and not rcv:
+            if self.state == self.PRIMED:
                 bit_time += 1
-                self.dut._log.debug(f"Waiting, bit time {bit_time//4}")
-                bit_time_max = 12.5
-                bit_time_acceptable = 7.5
-                if ((bit_time - 8) / 4.0) > bit_time_max + len(SYNC)/4:
+                self.dut._log.debug(f"Waiting, bit time {bit_time/4 -8}")
+                if (bit_time / 4.0) > bit_time_max + len(SYNC)/4:
                     self.dut._log.error(
                         "No data after {} bit times, which is more than {}".
-                        format(bit_time / 4.0, bit_time_max))
-                if ((bit_time - 8) / 4.0) > bit_time_acceptable + len(SYNC)/4:
+                        format(bit_time / 4.0 - 8, bit_time_max))
+                    raise TestFailure()
+
+            pkt += current()
+
+            if self.state == self.PRIMED and pkt == SYNC:
+                # Start monitoring
+                self.state = self.RECEIVING
+                self.dut._log.debug("Got SYNC")
+                if (bit_time / 4.0) > bit_time_acceptable + len(SYNC)/4:
                     self.dut._log.warn(
                         "No data after {} bit times (> {})".format(
-                            bit_time / 4.0, bit_time_acceptable))
-            pkt += current()
-            if self.primed and pkt == SYNC:
-                # Start monitoring
-                rcv = True
-                self.dut._log.debug("Got SYNC")
-                self.dut._log.info("Response came after {} bit times".format(
-                        (bit_time - len(SYNC)) / 4.0))
+                            bit_time / 4.0 - 8, bit_time_acceptable))
+                else:
+                    self.dut._log.info("Response came after {} bit times"
+                                       .format((bit_time - len(SYNC)) / 4.0))
                 bit_time = 0
                 continue
-            elif rcv and (pkt[-len(EOP):] == EOP):
+            elif self.state == self.RECEIVING and (pkt[-len(EOP):] == EOP):
                 # Pass the packet to listeners
                 self.dut._log.debug("Got EOP")
                 self.dut._log.debug("Current packet: [{}]".format(pkt))
                 self._recv(pkt)
                 pkt = ""
-                rcv = False
-                self.primed = False
-            elif (not rcv) and (len(pkt) >= len(SYNC)):
+                self.state = self.IDLE
+            elif (self.state != self.RECEIVING) and (len(pkt) >= len(SYNC)):
                 # Full window and nothing detected, slide it
                 pkt = pkt[1:]
             else:
