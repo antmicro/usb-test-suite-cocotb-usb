a29e2c13155b36e97d04ef5841b71edd5d3d6e51
Jędrzej Boczar <jboczar@antmicro.com>
2020-01-21 11:10:08 +0100

    send correct data on EP0 as response to IN tokens (cleanup required)

--- cocotb_usb/fx2/usb.py
+++ cocotb_usb/fx2/usb.py
@@ -46,6 +46,7 @@ def xram_mem_set(dut, adr, data):
             mem_name = 'mem_%s' % mem
             storage = getattr(dut, mem_name)
             storage[adr - origin].setimmediatevalue(data)
+            _dbg('xram_mem_set @0x%04x <= 0x%02x' % (adr, data))
             return
 
 
@@ -54,7 +55,24 @@ def xram_mem_get(dut, adr):
         if origin <= adr <= origin + size:
             mem_name = 'mem_%s' % mem
             storage = getattr(dut, mem_name)
+            _dbg('xram_mem_get @0x%04x => 0x%02x' % (adr, int(storage[adr - origin])))
             return int(storage[adr - origin])
+    raise KeyError('Address not in XRAM storage: 0x%04x' % adr)
+
+
+def setupdat_bytes_to_csr(setupdat):
+    # construct a SETUPDAT 64-bit value:
+    # (!) litex generates names with revesed numbers:
+    #   FX2 SETUPDAT[0] = setupdat7_w = fx2csr_setupdat[63:56]
+    setupdat_masks = [b << (8 * i) for i, b in enumerate(reversed(setupdat))]
+    setupdat64 = reduce(lambda acc, b: acc | b, setupdat_masks)
+    return setupdat64
+
+
+def setupdat_csr_to_bytes(setupdat64):
+    setupdat_bytes = [((setupdat64 & (0xff << (8 * i))) >> (8 * i)) for i in range(8)]
+    setupdat = list(reversed(setupdat_bytes))
+    return setupdat
 
 
 class Autopointers:
@@ -105,25 +123,21 @@ class Autopointers:
                 value = xram_mem_get(self.fx2usb.dut, self.autoptr1)
                 # set value of xautodat1 so that in next cycle it will be read on bus
                 self.fx2usb.set_csr('xautodat1', value, immediate=True)
-                print('xautodat1: read:  0x%02x @ 0x%04x' % (value, self.autoptr1))
                 if access.ack:
                     self.autoptr1 += 1
             else: # write
                 if access.ack:
                     xram_mem_set(self.fx2usb.dut, self.autoptr1, access.dat_w)
                     self.autoptr1 += 1
-                    print('xautodat1: write: 0x%02x @ 0x%04x' % (access.dat_w, self.autoptr1))
         elif access.adr == 0xe67c:  # XAUTODAT2
             if access.we == 0: # read (should be before ack!)
                 value = xram_mem_get(self.fx2usb.dut, self.autoptr2)
                 self.fx2usb.set_csr('xautodat2', value, immediate=True)
-                print('xautodat2: read:  0x%02x @ 0x%04x' % (value, self.autoptr2))
                 if access.ack:
                     self.autoptr2 += 1
             else: # write
                 if access.ack:
                     xram_mem_set(self.fx2usb.dut, self.autoptr2, access.dat_w)
-                    print('xautodat2: write: 0x%02x @ 0x%04x' % (access.dat_w, self.autoptr2))
                     self.autoptr2 += 1
 
 
@@ -171,8 +185,8 @@ class FX2USB:
         })
 
     def send_to_host(self, packet):
-        assert self.to_send is None
-        self.to_send = packet
+        _dbg('send_to_host(%s)' % packet)
+        self.expect_data_callback = lambda: packet
 
     def update_csr(self, name, *args, immediate=False, **kwargs):
         val = getattr(self.dut, 'fx2csr_' + name)
@@ -200,9 +214,10 @@ class FX2USB:
     ### Transaction state machine ##############################################
 
     def reset_state(self):
+        _dbg('reset_state')
         self.token_packet = None
-        self.to_send = None
         self.received_data_callback = None
+        self.expect_data_callback = None
         self.ack_callback = None
 
     class TransactionState(enum.Enum):
@@ -249,12 +264,11 @@ class FX2USB:
     def on_wait_data(self, s):
         p, S = self.packet, self.TransactionState
         if p.pid == PID.DATA0 or p.pid == PID.DATA1:  # as expected, do not handle DATA2/MDATA
-            if self.check_data_out_toggle(p):
-                self.received_data_callback(p)
-                self.send_to_host(handshake_packet(PID.ACK))
-            else:
+            if not self.check_data_out_toggle(p):
                 # TODO: wrong data sync
-                pass
+                _dbg('WRONG DATA SYNC')
+            self.received_data_callback(p)
+            self.send_to_host(handshake_packet(PID.ACK))
             # TODO: what if host does not receive ACK and sends data once again?
             return S.WAIT_TOKEN
         else:
@@ -295,20 +309,25 @@ class FX2USB:
             self.update_csr('ep0cs', setbits=[7], clearbits=[1, 0])
 
             def handle_setupdat(p):
-                # construct a SETUPDAT 64-bit value:
-                # (!) litex generates names with revesed numbers:
-                #   FX2 SETUPDAT[0] = setupdat7_w = fx2csr_setupdat[63:56]
-                setupdat = [b << (8 * i) for i, b in enumerate(reversed(p.data))]
-                setupdat64 = reduce(lambda acc, b: acc | b, setupdat)
-                self.set_csr('setupdat', setupdat64, immediate=True)
+                self.set_csr('setupdat', setupdat_bytes_to_csr(p.data), immediate=True)
                 # interrupt and acknowledge
                 self.assert_interrupt(self.IRQ.SUDAV)
 
+                # handle SET_ADDRESS requests, as firmware does not have to send any data back
+                # TODO: make this less hacky
+                if p.data[0] == 0x00 and p.data[1] == 0x05:
+                    _dbg('SET_ADDRESS: 0x%02x' % p.data[2])
+                    self.arm_endpoint(0)
+
             # during this callback we are sure that we have DATA0/DATA1
             self.received_data_callback = handle_setupdat
+
         elif p.pid == PID.OUT:
-            pass  # TODO
-            assert False
+            def handle_data(_):
+                nonlocal p
+                _dbg('Not implemented: handle_data_out: ep = %s' % p.ep)
+            self.received_data_callback = handle_data
+
         else:
             raise ValueError(p.pid)
 
@@ -318,16 +337,48 @@ class FX2USB:
         toggle = testbit(self.dut.togctl_toggles, ep2toggle_index(ep, io))
         data_pid = PID.DATA1 if toggle else PID.DATA0
         # TODO: send meaningful data
-        self.send_to_host(data_packet(data_pid, []))
-        def ack_callback(pid):
-            if pid == PID.ACK:
-                if toggle:
-                    self.dut.togctl_toggles = bitupdate(self.dut.togctl_toggles,
-                                                        clearbits=[ep2toggle_index(ep, io)])
-                else:
-                    self.dut.togctl_toggles = bitupdate(self.dut.togctl_toggles,
-                                                        setbits=[ep2toggle_index(ep, io)])
-        self.ack_callback = ack_callback
+
+        #  if ep == 0:
+        #      if testbit(self.get_csr('sudptrctl'), 0):  # SDPAUTO - automatic filling of EP buffer
+
+        #  if length is None:
+
+        count = 0
+        def expect_data_callback():
+            length = self.armed_ep_lengths[ep]
+            if length is None:
+                return None
+
+            nonlocal count
+            if count == 0:
+                count += 1
+                return None
+
+            # be sure to disarm the endpoint
+            self.armed_ep_lengths[ep] = None
+
+            if ep == 0:
+                origin = _ram_areas['ep0inout'][0]
+                data = [xram_mem_get(self.dut, origin + i) for i in range(length)]
+
+                _dbg('expect_data_callback/handle_token_in: data = %s' % data)
+                self.send_to_host(data_packet(data_pid, data))
+
+                def ack_callback(pid):
+                    if pid == PID.ACK:
+                        if toggle:
+                            self.dut.togctl_toggles = bitupdate(self.dut.togctl_toggles,
+                                                                clearbits=[ep2toggle_index(ep, io)])
+                        else:
+                            self.dut.togctl_toggles = bitupdate(self.dut.togctl_toggles,
+                                                                setbits=[ep2toggle_index(ep, io)])
+                self.ack_callback = ack_callback
+
+            else:
+                raise NotImplementedError('Endpoints other than 0 not implemented')
+
+        _dbg('handle_token_in')
+        self.expect_data_callback = expect_data_callback
 
     def check_data_out_toggle(self, p):
         tp = self.token_packet
@@ -346,6 +397,21 @@ class FX2USB:
 
     ### CPU register access monitor ############################################
 
+    def arm_endpoint(self, ep):
+        _dbg('Arming endpoint %d' % ep)
+        ep_len = lambda prefix: word(self.get_csr(prefix + 'h'), self.get_csr(prefix + 'l'))
+        if ep == 0:
+            length = ep_len('ep0bc')
+            if hasattr(self, '_force_length') and self._force_length is not None:
+                length = self._force_length
+                self._force_length = None
+            self.armed_ep_lengths[0] = length
+            print('self.armed_ep_lengths', end=' '); __import__('pprint').pprint(self.armed_ep_lengths)
+            self.update_csr('ep0cs', setbits=[1])
+        else:
+            raise NotImplementedError('Endpoints other than 0 not implemented')
+
+
     def xram_access_handler(self, access):
         if access.ack:
             # clear interrupt flags on writes instead of setting register value
@@ -360,17 +426,49 @@ class FX2USB:
                         # has already been written
                         self.set_csr(reg, bitupdate(last_val, clear=access.dat_w))
 
-            # endpoint arming
-            ep_len = lambda prefix: word(self.get_csr(prefix + 'h'), self.get_csr(prefix + 'l'))
-            if access.adr == self.csrs['ep0bcl']:
-                sdpauto = (self.get_csr('sudptrctl') & 0b1) != 0
+            # automatic data copying when writing sudptrl
+            if access.adr == self.csrs['sudptrl'] and access.we:
+                _dbg('SUDPTRL')
+
+                sdpauto = testbit(self.get_csr('sudptrctl'), 0)
                 if sdpauto:  # should get length from descriptors
-                    raise NotImplementedError()
-                else:
-                    self.armed_ep_lengths[0] = ep_len('ep0bc')
-                    # TODO: what when EP has already been armed?
-                # set BUSY bit in EP0CS
-                self.update_csr('ep0cs', setbits=[1])
+                    # the descriptor should be in our buffer
+                    adr_in = word(self.get_csr('sudptrh'), self.get_csr('sudptrl'))
+                    # first field should have length
+                    length = xram_mem_get(self.dut, adr_in)
+                    print('** length', end=' '); __import__('pprint').pprint(length)
+                    # copy data to endpoint buffer
+                    for i in range(length):
+                        adr = _ram_areas['ep0inout'][0] + i
+                        xram_mem_set(self.dut, adr, xram_mem_get(self.dut, adr_in + i))
+                    # update length
+                    self.set_csr('ep0bch', msb(length), immediate=True)
+                    self.set_csr('ep0bcl', lsb(length), immediate=True)
+                    self._force_length = length  # FIXME: we need to advance simulation or delay setting length
+                # now just arm endpoint
+                self.arm_endpoint(0)
+
+            # endpoint arming TODO: rewrite this, its now obsolete
+            if access.adr == self.csrs['ep0bcl'] and access.we:
+                _dbg('EP0BCL')
+                self.arm_endpoint(0)
+                #  sdpauto = (self.get_csr('sudptrctl') & 0b1) != 0
+                #  if sdpauto:  # should get length from descriptors
+                #      wLength = (self.get_csr('setupdat') & (0xffff << 4 * 8)) >> 4 * 8
+                #      print('wLength', end=' '); __import__('pprint').pprint(wLength)
+                #  else:
+                #      self.armed_ep_lengths[0] = ep_len('ep0bc')
+                #      # TODO: what when EP has already been armed?
+                #  # set BUSY bit in EP0CS
+                #  self.update_csr('ep0cs', setbits=[1])
+
+            # arming endopoint due to clearing HSNAK
+            if access.adr == self.csrs['ep0cs'] and access.we:
+                _dbg('EP0CS')
+                # clearing HSNAK by writing 1
+                if testbit(access.dat_w, 7):
+                    self.arm_endpoint(0)
+
 
         # even without ack handle autopointers
         self.autopointers.handle_xram_access(access)
@@ -388,12 +486,29 @@ class FX2USB:
 
     @cocotb.coroutine
     def expect_device_packet(self, timeout):
-        if self.to_send is not None:
-            packet = self.to_send
+        data = None
+
+        @cocotb.coroutine
+        def wait_data_to_send():
+            yield ClockCycles(self.dut.sys_clk, 1)
+            nonlocal data
+            while data is None:
+                if self.expect_data_callback is not None:
+                    data = self.expect_data_callback()
+                yield ClockCycles(self.dut.sys_clk, 1)
+
+        #  #  timer = Timer(timeout // 100)  # 10us, faster debugging
+        timer = Timer(timeout // 10)  # 10us, faster debugging
+        #  timer = Timer(timeout)
+        yield [timer, wait_data_to_send()]
+
+        if data is not None:
+            self.expect_data_callback = None
+            packet = data
             # simulate sending time
             yield ClockCycles(self.dut.sys_clk, len(wrap_packet(packet)))
             return packet
         else:
             #  yield Timer(timeout)
-            yield Timer(timeout // 100)  # 10us, faster debugging
+            #  yield Timer(timeout // 100)  # 10us, faster debugging
             return None
