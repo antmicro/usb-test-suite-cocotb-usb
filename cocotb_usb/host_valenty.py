0888b1b233224949fa31079c1fe60a6e3381405f
Sean Cross <sean@xobs.io>
2020-05-21 18:32:30 +0800

    host_vanelty: provide the test name to UsbTest
    Since this is a subclass of UsbTest, the test name always showed up as
    `get_harness()`. Provide the test name to the constructor, which was
    deriving an invalid test name based on crawling the stack.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- cocotb_usb/host_valenty.py
+++ cocotb_usb/host_valenty.py
@@ -10,7 +10,7 @@ from cocotb_usb.usb.packet import crc16
 from cocotb_usb.utils import grouper_tofit, parse_csr, assertEqual
 
 from cocotb_usb.host import UsbTest
-
+import inspect
 
 class UsbTestValenty(UsbTest):
     """Class for testing ValentyUSB IP core.
@@ -32,6 +32,7 @@ class UsbTestValenty(UsbTest):
         self.wb = WishboneMaster(dut, "wishbone", dut.clk12, timeout=20)
         self.csrs = dict()
         self.csrs = parse_csr(csr_file)
+        kwargs['test_name'] = inspect.stack()[2][3]
         super().__init__(dut, **kwargs)
 
     @cocotb.coroutine
